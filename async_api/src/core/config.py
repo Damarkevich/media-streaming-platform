1a8d26445df2a64e3f35137e0f5ee40a082a918a
Dmitry Markevich <markevich.dmitry@gmail.com>
2026-01-10 21:23:17 +0100

    feat: Update Dockerfile and configuration for async API, add new API routes, and modify docker-compose for improved service management

--- async_api/src/core/config.py
+++ async_api/src/core/config.py
@@ -6,12 +6,13 @@ from src.core.logger import LOGGING
 logging_config.dictConfig(LOGGING)
 
 PROJECT_NAME = os.getenv("PROJECT_NAME", "movies")
+
 REDIS_HOST = os.getenv("REDIS_HOST", "127.0.0.1")
 REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
 CACHE_EXPIRE_IN_SECONDS = int(os.getenv("CACHE_EXPIRE_IN_SECONDS", 300))
 
-ELASTIC_SCHEMA = os.getenv("ELASTIC_SCHEMA", "http://")
-ELASTIC_HOST = os.getenv("ELASTIC_HOST", "127.0.0.1")
-ELASTIC_PORT = int(os.getenv("ELASTIC_PORT", 9200))
+ELASTIC_SCHEMA = os.getenv("ES_SCHEMA", "http://")
+ELASTIC_HOST = os.getenv("ES_HOST", "127.0.0.1")
+ELASTIC_PORT = int(os.getenv("ES_PORT", 9200))
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))

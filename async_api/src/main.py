01b9d5ef29729e8c2af153eb60e06510fb921f09
Dmitry Markevich <markevich.dmitry@gmail.com>
2026-01-17 09:54:22 +0100

    feat: Include persons router in FastAPI application

--- async_api/src/main.py
+++ async_api/src/main.py
@@ -6,7 +6,7 @@ from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 from redis.asyncio import Redis
 
-from src.api.v1 import films, genres
+from src.api.v1 import films, genres, persons
 from src.core import config
 from src.db import elastic, redis
 
@@ -54,3 +54,4 @@ app = FastAPI(
 
 app.include_router(films.router, prefix="/api/v1/films", tags=["films"])
 app.include_router(genres.router, prefix="/api/v1/genres", tags=["genres"])
+app.include_router(persons.router, prefix="/api/v1/persons", tags=["persons"])

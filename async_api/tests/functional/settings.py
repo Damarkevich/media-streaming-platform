09f8502c22414eac6fe124116f2da7ba186ba53a
Dmitry Markevich <markevich.dmitry@gmail.com>
2026-02-12 23:29:28 +0100

    feat: Add Elasticsearch mapping configuration to es_mapping.py and refactor TestSettings to use it

--- async_api/tests/functional/settings.py
+++ async_api/tests/functional/settings.py
@@ -2,6 +2,8 @@ from typing import Any
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+from tests.functional.testdata.es_mapping import ES_MAPPING
+
 
 class TestSettings(BaseSettings):
     model_config = SettingsConfigDict(extra="ignore", env_file=".env")
@@ -26,85 +28,7 @@ class TestSettings(BaseSettings):
 
     @property
     def es_index_mapping(self) -> dict[str, Any]:
-        return {
-            "settings": {
-                "refresh_interval": "1s",
-                "analysis": {
-                    "filter": {
-                        "english_stop": {"type": "stop", "stopwords": "_english_"},
-                        "english_stemmer": {"type": "stemmer", "language": "english"},
-                        "english_possessive_stemmer": {
-                            "type": "stemmer",
-                            "language": "possessive_english",
-                        },
-                        "russian_stop": {"type": "stop", "stopwords": "_russian_"},
-                        "russian_stemmer": {"type": "stemmer", "language": "russian"},
-                    },
-                    "analyzer": {
-                        "ru_en": {
-                            "tokenizer": "standard",
-                            "filter": [
-                                "lowercase",
-                                "english_stop",
-                                "english_stemmer",
-                                "english_possessive_stemmer",
-                                "russian_stop",
-                                "russian_stemmer",
-                            ],
-                        }
-                    },
-                },
-            },
-            "mappings": {
-                "dynamic": "strict",
-                "properties": {
-                    "id": {"type": "keyword"},
-                    "imdb_rating": {"type": "float"},
-                    "title": {
-                        "type": "text",
-                        "analyzer": "ru_en",
-                        "fields": {"raw": {"type": "keyword"}},
-                    },
-                    "description": {"type": "text", "analyzer": "ru_en"},
-                    "directors_names": {"type": "text", "analyzer": "ru_en"},
-                    "actors_names": {"type": "text", "analyzer": "ru_en"},
-                    "writers_names": {"type": "text", "analyzer": "ru_en"},
-                    "genres_names": {"type": "text", "analyzer": "ru_en"},
-                    "genres": {
-                        "type": "nested",
-                        "dynamic": "strict",
-                        "properties": {
-                            "id": {"type": "keyword"},
-                            "name": {"type": "text", "analyzer": "ru_en"},
-                        },
-                    },
-                    "directors": {
-                        "type": "nested",
-                        "dynamic": "strict",
-                        "properties": {
-                            "id": {"type": "keyword"},
-                            "full_name": {"type": "text", "analyzer": "ru_en"},
-                        },
-                    },
-                    "actors": {
-                        "type": "nested",
-                        "dynamic": "strict",
-                        "properties": {
-                            "id": {"type": "keyword"},
-                            "full_name": {"type": "text", "analyzer": "ru_en"},
-                        },
-                    },
-                    "writers": {
-                        "type": "nested",
-                        "dynamic": "strict",
-                        "properties": {
-                            "id": {"type": "keyword"},
-                            "full_name": {"type": "text", "analyzer": "ru_en"},
-                        },
-                    },
-                },
-            },
-        }
+        return ES_MAPPING
 
 
 test_settings = TestSettings()

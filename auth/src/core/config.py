cab2bea33eb57abb16887a59e20b9f9481db1064
Dmitry Markevich <markevich.dmitry@gmail.com>
2026-03-01 14:36:09 +0100

    fix: Update access token expiration to 5 minutes in configuration files

--- auth/src/core/config.py
+++ auth/src/core/config.py
@@ -1,6 +1,5 @@
 from logging import config as logging_config
 
-from pydantic import model_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from src.core.logger import LOGGING
@@ -22,12 +21,11 @@ class Settings(BaseSettings):
     authjwt_header_type: str = "Bearer"
     authjwt_denylist_enabled: bool = True
     authjwt_denylist_token_checks: set[str] = {"access", "refresh"}
-    access_token_expires: int = 60 * 60  # 1 hour in seconds
+    access_token_expires: int = 60 * 5  # 5 minutes in seconds
     refresh_token_expires: int = 60 * 60 * 24 * 7  # 7 days in seconds
 
     redis_host: str = "127.0.0.1"
     redis_port: int = 6379
-    cache_expire_in_seconds: int = 300  # 5 minutes in seconds
 
     postgres_db: str = "auth_database"
     postgres_user: str = "app"

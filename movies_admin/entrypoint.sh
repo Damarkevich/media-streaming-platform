7d8cc6c003d858511d839293cf1c45d8c6ef51de
Dmitry Markevich <markevich.dmitry@gmail.com>
2026-03-22 20:21:18 +0100

    feat(env): add environment configuration for movies_admin and update auth API login URL
    feat(config): enhance Nginx configuration with new API routes for content and auth
    
    fix(docker): remove redundant collectstatic command from Dockerfile
    
    fix(entrypoint): ensure collectstatic command is executed in entrypoint script

--- movies_admin/entrypoint.sh
+++ movies_admin/entrypoint.sh
@@ -6,5 +6,8 @@ uv run manage.py makemigrations --noinput
 # Run database migrations
 uv run manage.py migrate --noinput
 
+# Collect static files
+uv run manage.py collectstatic --noinput
+
 # Start gunicorn
 exec uv run gunicorn config.wsgi --bind 0.0.0.0:8000
